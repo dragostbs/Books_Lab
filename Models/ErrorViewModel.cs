5a058cda69784975a4bc674f1bc69be1027a169b
DRAGOȘ BOSCAN <dragos.boscan1@stud.ubbcluj.ro>
2021-10-11 15:16:34 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dragos_Boscan_L2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
