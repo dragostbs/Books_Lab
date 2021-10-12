b43130083eb9ebf42f12b2c72d9406993f7a1577
DRAGOȘ BOSCAN <dragos.boscan1@stud.ubbcluj.ro>
2021-10-12 21:10:08 +0300

    Dragos_Boscan_Lab3

--- Models/Order.cs
+++ Models/Order.cs
@@ -10,6 +10,7 @@ namespace Dragos_Boscan_L2.Models
         public int OrderID { get; set; }
         public int CustomerID { get; set; }
         public int BookID { get; set; }
+        public DateTime OrderDate { get; set; }
 
         public Customer Customer { get; set; }
         public Book Book { get; set; }
